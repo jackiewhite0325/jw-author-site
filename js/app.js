f21316e305eae6c101c8f99f8376cecc28c7a550
jackiewhite0325 <jackiewhite0325@gmail.com>
2026-08-17 10:21:41 -0700

    Delete js/app.js

--- js/app.js
+++
@@ -1,4 +0,0 @@
-// DEPRECATED: This file is no longer used.
-// All functionality has been moved to /app.js
-// This file is kept for historical reference only.
-console.warn('js/app.js is deprecated. Using /app.js instead.');
